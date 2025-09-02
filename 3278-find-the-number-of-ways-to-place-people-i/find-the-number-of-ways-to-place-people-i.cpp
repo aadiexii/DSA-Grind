1a539254778f4bbc447f66526a7bbb8fe6d01a27
Shivam Sharma <150658600+aadiexii@users.noreply.github.com>
2025-09-03 04:20:35 +0530

    Time: 3 ms (76.09%) | Memory: 32.5 MB (72.46%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -5,38 +5,66 @@ public:
      int result = 0;
 
      //Brute Approach
-     for(int i=0; i<n; i++){
-        // A
-        int x1 = points[i][0];
-        int y1 = points[i][1];
+    //  for(int i=0; i<n; i++){
+    //     // A
+    //     int x1 = points[i][0];
+    //     int y1 = points[i][1];
 
-        for(int j=0; j<n; j++){
-            if(i == j) continue;
+    //     for(int j=0; j<n; j++){
+    //         if(i == j) continue;
 
-            //B
-            int x2 = points[j][0];
-            int y2 = points[j][1];
+    //         //B
+    //         int x2 = points[j][0];
+    //         int y2 = points[j][1];
 
-            if(x2 >= x1 && y2 <= y1){
+    //         if(x2 >= x1 && y2 <= y1){
 
-                bool isInsidePoint = false;
-                for(int k=0; k<n; k++){
-                    if(k == i || k == j) continue;
+    //             bool isInsidePoint = false;
+    //             for(int k=0; k<n; k++){
+    //                 if(k == i || k == j) continue;
 
-                    int x3 = points[k][0];
-                    int y3 = points[k][1];
+    //                 int x3 = points[k][0];
+    //                 int y3 = points[k][1];
 
-                    if(x3 >= x1 && x3 <= x2 && y3 <= y1 && y3 >= y2){
-                        isInsidePoint = true;
-                        break;
-                    }
-                }
-            if(!isInsidePoint){
-                result++;
-            }
-            }
+    //                 if(x3 >= x1 && x3 <= x2 && y3 <= y1 && y3 >= y2){
+    //                     isInsidePoint = true;
+    //                     break;
+    //                 }
+    //             }
+    //         if(!isInsidePoint){
+    //             result++;
+    //         }
+    //         }
+    //     }
+    //  }
+    // return result;
+
+    sort(points.begin(), points.end(), [](vector<int> &points1,vector<int> &points2){
+        if(points1[0] == points2[0]){
+            return points1[1] > points2[1];
         }
-     }
+        return points1[0] < points2[0];
+    });
+
+        for(int i=0; i<n; i++){
+            //A
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            
+            int maxY = INT_MIN;
+            for(int j=i+1; j<n; j++){
+                //B
+                int x2 = points[j][0];
+                int y2 = points[j][1];   
+
+                if(y2 > y1) continue;
+                if(maxY < y2){
+                    result++;
+                    maxY = y2;
+                }    
+            }
+  
+        }
     return result;
     }
 };
