ae7ccab3e95260dbedb34723a9fbebc11677b907
Shivam Sharma <150658600+aadiexii@users.noreply.github.com>
2025-09-03 03:46:45 +0530

    Time: 9 ms (60.87%) | Memory: 32.6 MB (72.46%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+     int n = points.size();
+     int result = 0;
+
+     //Brute Approach
+     for(int i=0; i<n; i++){
+        // A
+        int x1 = points[i][0];
+        int y1 = points[i][1];
+
+        for(int j=0; j<n; j++){
+            if(i == j) continue;
+
+            //B
+            int x2 = points[j][0];
+            int y2 = points[j][1];
+
+            if(x2 >= x1 && y2 <= y1){
+
+                bool isInsidePoint = false;
+                for(int k=0; k<n; k++){
+                    if(k == i || k == j) continue;
+
+                    int x3 = points[k][0];
+                    int y3 = points[k][1];
+
+                    if(x3 >= x1 && x3 <= x2 && y3 <= y1 && y3 >= y2){
+                        isInsidePoint = true;
+                        break;
+                    }
+                }
+            if(!isInsidePoint){
+                result++;
+            }
+            }
+        }
+     }
+    return result;
+    }
+};
